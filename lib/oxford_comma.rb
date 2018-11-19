6c3f04facfbbec5d116cf52243051588f163148e
dwolfe1987 <dwolfe1987@gmail.com>
2018-11-19 01:14:23 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
-  if array.length == 1  
+  if array.length == 2  
     array.join (" and ")
-  elsif array.length > 1 
+  elsif array.length > 2  
   array[-1].insert(0, "and ")
   end
 end
